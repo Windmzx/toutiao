99d05c4807ec59b201eb24e4072d8cb5c3410d63
Windmzx <2281927774@qq.com>
2017-04-07 13:44:41 +0800

    登陆注册已经完成

---
+++ src/main/java/com/newcoder/controller/LoginController.java
@@ -0,0 +1,43 @@
+package com.newcoder.controller;
+
+import com.newcoder.service.UserService;
+import com.newcoder.utils.ServiceUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Map;
+
+/**
+ * Created by mzx on 17.4.7.
+ */
+@Controller
+public class LoginController {
+
+    @Autowired
+    private UserService userService;
+
+
+    @ResponseBody
+    @RequestMapping(path = "/login", method = {RequestMethod.GET})
+    public String login(@RequestParam("username") String username,
+                        @RequestParam("password") String password,
+                        @RequestParam(value = "rember", defaultValue = "0") int rember,
+                        HttpServletResponse response) {
+
+        Map<String, Object> map = userService.login(username, password, rember);
+        //返回的map里有ticket说明 登陆成功
+        if (map.containsKey("ticket")) {
+            Cookie cookie = new Cookie("ticket", map.get("ticket").toString());
+            cookie.setPath("/");
+            response.addCookie(cookie);
+            return ServiceUtil.getJson(0, "登陆成功");
+        }
+        return ServiceUtil.getJson(1, map);
+    }
+}
